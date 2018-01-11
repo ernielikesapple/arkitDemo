c888a000ff5ea69136c8758c8811df890e8c64db
ernie.cheng <ernie.cheng@perficient.com>
2018-01-11 22:46:25 +0800

    multithread part2 (with bug not fix yet)

--- ARKitDemo/multithreadDemo/PhotoListTableViewController.swift
+++ ARKitDemo/multithreadDemo/PhotoListTableViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 import CoreImage
 
-let dataSourceURL = NSURL(string: "http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist")
+let dataSourceURL = "http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist"
 
 class PhotoListTableViewController: UITableViewController {
     
-    lazy var photos = NSDictionary(contentsOf: dataSourceURL! as URL)!
+    //  lazy var photos = NSDictionary(contentsOf: dataSourceURL! as URL)!
+    var photos = [PhotoRecord]()
+    let pendingOperations = PendingOperations()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Classis Photos"
+        fetchPhotoDetails()
     }
     
     // MARK: - Table view data source
@@ -28,33 +31,137 @@ class PhotoListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
-        let rowKey = photos.allKeys[indexPath.row] as! String
-        var image: UIImage?
+        if cell.accessoryView == nil {
+            cell.accessoryView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        }
+        // let rowKey = photos.allKeys[indexPath.row] as! String
+        /* var image: UIImage?
         if let imageURL = NSURL(string:photos[rowKey] as! String), let imageData = NSData(contentsOf: imageURL as URL) {
             // 1
             let unfilteredImage = UIImage(data: imageData as Data)
             // 2
             image = self.appySepialFilter(image: unfilteredImage!)
         }
-        
         cell.textLabel?.text = rowKey
         if let image = image {
          cell.imageView?.image = image
+        } */
+        cell.textLabel?.text = self.photos[indexPath.row].name
+        cell.imageView?.image = self.photos[indexPath.row].image
+        let indicator = cell.accessoryView as! UIActivityIndicatorView
+        switch photos[indexPath.row].state {
+        case .Filtered:
+            indicator.stopAnimating()
+        case .Failed:
+            indicator.stopAnimating()
+            cell.textLabel?.text = "---"
+        case .New, .Downloaded:
+            indicator.stopAnimating()
+            self.startOperationsForPhotoRecord(photoDetails: photos[indexPath.row], indexPath: indexPath)
         }
-        
         return cell
     }
     
-    func appySepialFilter(image: UIImage) -> UIImage {
-        let inputImage = CIImage(data: UIImagePNGRepresentation(image)!)
-        let context = CIContext(options: nil)
-        let filter = CIFilter(name: "CISepiaTone")
-        filter?.setValue(inputImage, forKey: kCIInputImageKey)
-        filter?.setValue(0.8, forKey: "inputIntensity")
-        if let outputImage = filter?.outputImage {
-            let outImage = context.createCGImage(outputImage, from: outputImage.extent)
-            return UIImage(cgImage: outImage!)
+    func startOperationsForPhotoRecord(photoDetails: PhotoRecord, indexPath: IndexPath) {
+        switch (photoDetails.state) {
+        case .New:
+            startDownloadForRecord(photoDetails: photoDetails, indexPath: indexPath)
+        case .Downloaded:
+            startFiltrationForRecord(photoDetails: photoDetails, indexPath: indexPath)
+        default:
+            print("")
         }
-        return UIImage()
     }
+    
+    func startDownloadForRecord(photoDetails: PhotoRecord, indexPath: IndexPath) {
+        // 1 to see if there is already an operation in downloads in progress for it
+        if let downloadOperation = pendingOperations.downloadsInProgress[indexPath] {
+            return
+        }
+        // 2 create an instance of image downloader by using designated initial either
+        let downloader = ImageDownloader(photoRecord: photoDetails)
+        // 3
+        downloader.completionBlock = {
+            if downloader.isCancelled {
+                return
+            }
+            DispatchQueue.main.async {
+                self.pendingOperations.downloadsInProgress.removeValue(forKey: indexPath)
+                self.tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        }
+        // 4
+        pendingOperations.downloadsInProgress[indexPath] = downloader
+        // 5
+        pendingOperations.downloadQueue.addOperation(downloader)
+    }
+    
+    func startFiltrationForRecord(photoDetails: PhotoRecord, indexPath: IndexPath) {
+        if let filterOperation = pendingOperations.filtrationsInProgress[indexPath] {
+            return
+        }
+        let filterer = ImageFiltration(photoRecord: photoDetails)
+        filterer.completionBlock = {
+            if filterer.isCancelled {
+                return
+            }
+            DispatchQueue.main.async {
+                self.pendingOperations.filtrationsInProgress.removeValue(forKey: indexPath)
+                self.tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        }
+        pendingOperations.filtrationsInProgress[indexPath] = filterer
+        pendingOperations.filtrationQueue.addOperation(filterer)
+    }
+    
+    func fetchPhotoDetails() {
+        // UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        guard let url = URL(string: dataSourceURL) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        // set up the session
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        // make the request
+        let task =  session.dataTask(with: urlRequest) { (data, response, error) in
+            guard error == nil else {
+                print("error at session data task")
+                return
+            }
+            guard let responseData = data else {
+                print("Error: did not reive data")
+                return
+            }
+            do {
+            // You should unwrap your data object instead of checking it against nil. Also you need to implement do try catch error handling instead of forcing it:
+                let datasourceDictionary = try PropertyListSerialization.propertyList(from: responseData, options: PropertyListSerialization.MutabilityOptions.mutableContainersAndLeaves, format: nil) as! NSDictionary
+                for (key, value) in datasourceDictionary {
+                    if let imageName = key as? String, let imageUrl = URL(string: value as! String) {
+                        let photoRecord = PhotoRecord(name: imageName,url: imageUrl)
+                        self.photos.append(photoRecord)
+                    }
+                }
+            } catch {
+                print("error at parsing data")
+                return
+            }
+            // UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+        task.resume()
+    }
+    
+//    func appySepialFilter(image: UIImage) -> UIImage {
+//        let inputImage = CIImage(data: UIImagePNGRepresentation(image)!)
+//        let context = CIContext(options: nil)
+//        let filter = CIFilter(name: "CISepiaTone")
+//        filter?.setValue(inputImage, forKey: kCIInputImageKey)
+//        filter?.setValue(0.8, forKey: "inputIntensity")
+//        if let outputImage = filter?.outputImage {
+//            let outImage = context.createCGImage(outputImage, from: outputImage.extent)
+//            return UIImage(cgImage: outImage!)
+//        }
+//        return UIImage()
+//    }
 }
---
+++ ARKitDemo/multithreadDemo/PhotoOperations.swift
@@ -0,0 +1,127 @@
+//
+//  PhotoOperations.swift
+//  ARKitDemo
+//
+//  Created by ernie.cheng on 1/10/18.
+//  Copyright © 2018 ernie.cheng. All rights reserved.
+//
+
+import UIKit
+
+// represent each photo displayed in the app,
+enum PhotoRecordState {
+    case New, Downloaded, Filtered, Failed
+}
+
+class PhotoRecord {
+    let name: String?
+    let url: URL?
+    var state = PhotoRecordState.New
+    var image = UIImage(named: "Placeholder")
+    
+    init(name: String?, url: URL?) {
+        self.name = name
+        self.url = url
+    }
+}
+
+// to tack the status of each operation
+// contains two dictionaries to keep track of active and pending download and filter operations for each row in the table
+// and two operation queues for each type of operation
+class PendingOperations {
+    lazy var downloadsInProgress = [IndexPath:Operation]()
+    lazy var downloadQueue:OperationQueue = {
+       var queue = OperationQueue()
+        queue.name = "Download queue"
+        // queue.maxConcurrentOperationCount = 1
+        //        The maxConcurrentOperationCount is set to 1 here for the sake of this tutorial, to allow you to see operations finishing one by one. You could leave this part out to allow the queue to decide how many operations it can handle at once – this would further improve performance.
+        //        How does the queue decide how many operations it can run at once? That’s a good question! :] It depends on the hardware. By default, NSOperationQueue will do some calculation behind the scenes, decide what is best for the particular platform the code is running on, and will launch the maximum possible number of threads.
+        return queue
+    }()
+    
+    lazy var filtrationsInProgress = [IndexPath:Operation]()
+    lazy var filtrationQueue: OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Image Filtration queue"
+        // queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+}
+// why you have to keep track of all active and pending operations. The queue has an operations method which returns an array of operations, so why not use that? In this project it won’t be very efficient to do so. You need to track which operations are associated with which table view rows, which would involve iterating over the array each time you needed one. Storing them in a dictionary with the index path as a key means lookup is fast and efficient
+
+class ImageDownloader: Operation {
+    let photoRecord: PhotoRecord
+    // 1 add a constant reference to the PhotoRecord object related to the operation
+    init(photoRecord: PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    // override the main method in Operation subclass to actually perform work
+    override func main() {
+        // check for cancellation before staring. Operations should regularly check if they have been cancelled before attempting long or intensive work
+        guard self.isCancelled else {
+            return
+        }
+        // Download the image data
+        var imageData = Data()
+        if let url = self.photoRecord.url {
+            do {
+                imageData = try Data(contentsOf: url)
+            } catch {
+                print("there is no image data")
+            }
+        }
+        // check again for cancellation
+        guard self.isCancelled else {
+            return
+        }
+        // if there is data, create an image object and add it to the record and move the state along, if there is no data, mark the record as failed and set the appropriate image.
+        if imageData.count > 0 {
+            self.photoRecord.image = UIImage(data: imageData as Data)
+            self.photoRecord.state = .Downloaded
+        } else {
+            self.photoRecord.state = .Failed
+            self.photoRecord.image = UIImage(named: "Failed")
+        }
+    }
+}
+
+class ImageFiltration: Operation {
+    let photoRecord: PhotoRecord
+    init(photoRecord: PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    override func main() {
+        guard self.isCancelled else {
+            return
+        }
+        if self.photoRecord.state != .Downloaded {
+            return
+        }
+        if let filteredImage = self.appySepialFilter(image: self.photoRecord.image) {
+            self.photoRecord.image = filteredImage
+            self.photoRecord.state = .Filtered
+        }
+    }
+    
+    func appySepialFilter(image: UIImage?) -> UIImage? {
+        guard self.isCancelled else {
+            return nil
+        }
+        if let image = image {
+            let inputImage = CIImage(data: UIImagePNGRepresentation(image)!)
+            let context = CIContext(options: nil)
+            let filter = CIFilter(name: "CISepiaTone")
+            filter?.setValue(inputImage, forKey: kCIInputImageKey)
+            filter?.setValue(0.8, forKey: "inputIntensity")
+            if let outputImage = filter?.outputImage {
+                guard self.isCancelled else { // do cancellation check before and after any expensive method call.
+                    return nil
+                }
+                let outImage = context.createCGImage(outputImage, from: outputImage.extent)
+                return UIImage(cgImage: outImage!)
+            }
+        }
+        return UIImage()
+    }
+}
+
